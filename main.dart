d63a08e18d0b9e7c48d0e4e8075322e1e391ecde
Zohaib Jamal <143255118+Zohaib-Jamal@users.noreply.github.com>
2023-09-11 12:04:12 +0500

    main.dart

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: WebViewApp(),
+  ));
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp();
+
+  @override
+  _WebViewAppState createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  bool _isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebView(
+              initialUrl: "https://virtumart.site/",
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (String url) {
+                setState(() {
+                  _isLoading = false;
+                });
+              },
+            ),
+            if (_isLoading)
+              Container(
+                color: Colors.white, // White background color
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
